6a97e9cc07d351b495bff525ebe772ebeaedb5c2
Nazar <53997458+NazarWild@users.noreply.github.com>
2020-10-10 22:19:06 +0300

    nd first commit

--- app/src/mainwindow.cpp
+++ app/src/mainwindow.cpp
@@ -16,5 +16,40 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_actionOpen_triggered()
 {
-    std::cout << "lolchik\n";
+  QFileDialog dialog(this);
+  dialog.setDirectory(QDir::homePath());
+  dialog.setFileMode(QFileDialog::Directory);
+  QString filename = dialog.getOpenFileName();
+  QFile file(filename);
+  if (!file.open(QFile::ReadOnly | QFile::Text)) {
+    QMessageBox::warning(this, "alarm", "file not open");
+  }
+  QTextStream in(&file);
+  QString text = in.readAll();
+  ui->textEdit->setPlainText(text);
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    std::cout << "Saved\n";
+}
+
+void MainWindow::on_actionTheme_triggered()
+{
+    std::cout << "Theme\n";
+}
+
+void MainWindow::on_actionHard_Mode_triggered()
+{
+    std::cout << "HardMode\n";
+}
+
+void MainWindow::on_actionHealth_Care_Mode_triggered()
+{
+    std::cout << "HealthCareMode\n";
+}
+
+void MainWindow::on_actionNormal_Mode_triggered()
+{
+    std::cout << "NormalMode\n";
 }
--- app/src/mainwindow.h
+++ app/src/mainwindow.h
@@ -1,7 +1,13 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QMenuBar>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QDir>
 #include <QApplication>
+#include <QMessageBox>
+#include "main_lib.h"
 
 namespace Ui {
   class MainWindow;
@@ -18,6 +24,16 @@ public:
 private slots:
   void on_actionOpen_triggered();
 
+  void on_actionHard_Mode_triggered();
+
+  void on_actionSave_triggered();
+  
+  void on_actionTheme_triggered();
+  
+  void on_actionHealth_Care_Mode_triggered();
+  
+  void on_actionNormal_Mode_triggered();
+  
 private:
   Ui::MainWindow *ui;
 };
