7f24ac5f76007ee0e77c0f26e2c81dbd38e6a480
Nazar <53997458+NazarWild@users.noreply.github.com>
2020-10-19 14:47:10 +0300

    without build

--- build/app/src/CMakeFiles/liba_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/liba_autogen"
-  "liba_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/liba_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
